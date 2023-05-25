d8158b7343d77b4e7cbbfdbea73e661577a9fe21
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-05-25 14:54:45 +0300

    function fetchBreeds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const API_KEY = "live_a8DViAR1Xqq26ArqqNz0CAF7sHdqahYGHoDQoa3eoa4T1qCsSxymmNqqJh2aOsgi";
+const BASE_URL = "https://api.thecatapi.com/v1/";
+
+
+
 const refs = {
-  breedSelect : document.querySelector('.breed-select')
+  breedSelect : document.querySelector('.breed-select'),
+  loader : document.querySelector(".loader"),
+  error : document.querySelector(".error"),
+  catInfo : document.querySelector(".cat-info")
+}
+
+function fetchBreeds() {
+  const option = new URLSearchParams({
+
+  })
+  return fetch(`${BASE_URL}breeds?api key=${API_KEY}`)
 }
